50006ba581e19dfba00d4015cb14ea3c1c3354bd
Sjin618 <tkdsk740@gmail.com>
2026-03-11 18:32:42 +0900

    feat: 좋아요 추가

---
+++ src/main/java/com/acc/gazua/domain/like/controller/LikeController.java
@@ -0,0 +1,9 @@
+package com.acc.gazua.domain.like.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LikeController {
+}
---
+++ src/main/java/com/acc/gazua/domain/like/service/LikeService.java
@@ -0,0 +1,9 @@
+package com.acc.gazua.domain.like.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+}
